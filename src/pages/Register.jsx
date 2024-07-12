3f0ca330c866d2ec1c0f0defd52a4e47326334be
Diego Guaman <diegoguaman05@hotmail.com>
2024-07-12 19:55:21 +0200

    manejo de errores register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,9 +4,11 @@ import Input from "../components/Input/Input";
 import FormWizard from "react-form-wizard-component";
 import "react-form-wizard-component/dist/style.css";
 import { useNavigate } from "react-router-dom";
+import PacmanLoading from "../components/PacmanLoading/PacmanLoading";
 
 const Register = () => {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
   const [user, setUser] = useState({
     name: "",
     email: "",
@@ -21,6 +23,7 @@ const Register = () => {
   });
 
   const [isLastStep, setIsLastStep] = useState(false);
+  const [error, setError] = useState("");
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -31,11 +34,20 @@ const Register = () => {
   };
 
   const handleComplete = () => {
+    setLoading(true);
     createUser(user)
       .then(() => {
         navigate("/login");
       })
-      .catch((err) => console.error(err));
+      .catch((err) => {
+        console.log("ERRROR", err);
+        const errorMessage =
+          err.message || "An error occurred, please try again.";
+
+        console.log("errorMessage", errorMessage);
+        setError(errorMessage);
+      })
+      .finally(() => setLoading(false));
   };
 
   const tabChanged = ({ prevIndex, nextIndex }) => {
@@ -74,149 +86,162 @@ const Register = () => {
 
   return (
     <>
-      <FormWizard
-        shape="circle"
-        color="#83A580"
-        style={{ borderColor: "#83A580" }}
-        onComplete={handleComplete}
-        onSubmit={handleComplete}
-        onTabChange={tabChanged}
-      >
-        <FormWizard.TabContent title="Datos personales" icon="ti-user">
-          <h2>Tus datos</h2>
-          <Input
-            value={user.name}
-            onChange={handleInputChange}
-            name="name"
-            type="text"
-            title="Nombre"
-          />
-          <Input
-            value={user.email}
-            onChange={handleInputChange}
-            name="email"
-            type="email"
-            title="Email"
-          />
-          <Input
-            value={user.password}
-            onChange={handleInputChange}
-            name="password"
-            type="password"
-            title="Contraseña"
-          />
-          <select
-            className="form-select"
-            aria-label="Default select example"
-            name="gender"
-            value={user.gender}
-            onChange={handleInputChange}
+      {loading ? (
+        <PacmanLoading />
+      ) : (
+        <>
+          <FormWizard
+            shape="circle"
+            color="#83A580"
+            style={{ borderColor: "#83A580" }}
+            onComplete={handleComplete}
+            onSubmit={handleComplete}
+            onTabChange={tabChanged}
           >
-            <option selected>Selecciona el género</option>
-            <option value="masculino">Masculino</option>
-            <option value="femenino">Femenino</option>
-            <option value="otro">Otro</option>
-          </select>
-        </FormWizard.TabContent>
-        <FormWizard.TabContent title="Detalles personales" icon="ti-heart">
-          <h2>Objetivo</h2>
-          <p>Información para tu plan de ejercicios</p>
-          <Input
-            value={user.weight}
-            onChange={handleInputChange}
-            name="weight"
-            type="number"
-            title="Tu peso"
-            placeholder="En kilogramos"
-          />
-          <Input
-            value={user.height}
-            onChange={handleInputChange}
-            name="height"
-            type="number"
-            title="Tu altura"
-            placeholder="En centímetros"
-          />
-          <label htmlFor="objetive" className="form-label">
-            ¿Cuál es tu objetivo principal?
-          </label>
-          <select
-            className="form-select"
-            aria-label="Default select example"
-            name="objetive"
-            value={user.objetive}
-            onChange={handleInputChange}
-          >
-            <option selected>Elige tu objetivo</option>
-            <option value="comer equilibrado">Comer equilibrado</option>
-            <option value="perder peso">Perder peso</option>
-            <option value="ganar músculo">Ganar músculo</option>
-          </select>
-        </FormWizard.TabContent>
-        <FormWizard.TabContent title="Plan de alimentación" icon="ti-star">
-          <h1>Tu Dieta</h1>
-          <p>Información para tu dieta</p>
-          <select
-            className="form-select mb-4"
-            aria-label="Default select example"
-            name="ability"
-            value={user.ability}
-            onChange={handleInputChange}
-          >
-            <option selected>Tu habilidad en la cocina</option>
-            <option value="bajo">Bajo: El colacao cuenta como cocinar</option>
-            <option value="medio">Medio: Las lentejas no se me queman</option>
-            <option value="avanzado">
-              Avanzado: David muñoz a mi lado es un mindundi
-            </option>
-          </select>
-          <select
-            className="form-select mb-4"
-            aria-label="Default select example"
-            name="typeDiet"
-            value={user.typeDiet}
-            onChange={handleInputChange}
-          >
-            <option selected>Tipo de dieta</option>
-            <option value="omnivoro">Omnivoro: como carne y casi todo</option>
-            <option value="flexitariano">
-              Flexitariano: no excluyo la carne del todo{" "}
-            </option>
-            <option value="vegetariano">Vegetariano</option>
-            <option value="vegano">Vegano</option>
-            <option value="otra">Otra</option>
-          </select>
-          <label htmlFor="objetive" className="form-label">
-            ¿Eres alérgico o intelerante a algún alimento?
-          </label>
-          <select
-            className="form-select mb-4"
-            aria-label="Default select example"
-            name="alergic"
-            value={user.alergic}
-            onChange={handleInputChange}
-          >
-            <option selected>Tipo de alimento</option>
-            <option value="ninguno">Ninguno</option>
-            <option value="huevo">Huevo</option>
-            <option value="marisco">Marisco</option>
-            <option value="lactosa">Lactosa</option>
-            <option value="gluten">Gluten</option>
-            <option value="frutos secos">Frutos secos</option>
-          </select>
-        </FormWizard.TabContent>
-        <FormWizard.TabContent title="Validación" icon="ti-check">
-          <h1>¡Enhorabuena! ¡Has completado todos los pasos!</h1>
-          <p>¡Estás en buenas manos! y no nos las lavamos a menudo</p>
-          <quote>
-            "Las buenas recetas son como los buenos amigos: hacen que la vida
-            sea más deliciosa"
-          </quote>
-        </FormWizard.TabContent>
-      </FormWizard>
-      <style>{`
+            <FormWizard.TabContent title="Datos personales" icon="ti-user">
+              <h2>Tus datos</h2>
+              <Input
+                value={user.name}
+                onChange={handleInputChange}
+                name="name"
+                type="text"
+                title="Nombre"
+              />
+              <Input
+                value={user.email}
+                onChange={handleInputChange}
+                name="email"
+                type="email"
+                title="Email"
+              />
+              <Input
+                value={user.password}
+                onChange={handleInputChange}
+                name="password"
+                type="password"
+                title="Contraseña"
+              />
+              <select
+                className="form-select"
+                aria-label="Default select example"
+                name="gender"
+                value={user.gender}
+                onChange={handleInputChange}
+              >
+                <option selected>Selecciona el género</option>
+                <option value="masculino">Masculino</option>
+                <option value="femenino">Femenino</option>
+                <option value="otro">Otro</option>
+              </select>
+            </FormWizard.TabContent>
+            <FormWizard.TabContent title="Detalles personales" icon="ti-heart">
+              <h2>Objetivo</h2>
+              <p>Información para tu plan de ejercicios</p>
+              <Input
+                value={user.weight}
+                onChange={handleInputChange}
+                name="weight"
+                type="number"
+                title="Tu peso"
+                placeholder="En kilogramos"
+              />
+              <Input
+                value={user.height}
+                onChange={handleInputChange}
+                name="height"
+                type="number"
+                title="Tu altura"
+                placeholder="En centímetros"
+              />
+              <label htmlFor="objetive" className="form-label">
+                ¿Cuál es tu objetivo principal?
+              </label>
+              <select
+                className="form-select"
+                aria-label="Default select example"
+                name="objetive"
+                value={user.objetive}
+                onChange={handleInputChange}
+              >
+                <option selected>Elige tu objetivo</option>
+                <option value="comer equilibrado">Comer equilibrado</option>
+                <option value="perder peso">Perder peso</option>
+                <option value="ganar músculo">Ganar músculo</option>
+              </select>
+            </FormWizard.TabContent>
+            <FormWizard.TabContent title="Plan de alimentación" icon="ti-star">
+              <h1>Tu Dieta</h1>
+              <p>Información para tu dieta</p>
+              <select
+                className="form-select mb-4"
+                aria-label="Default select example"
+                name="ability"
+                value={user.ability}
+                onChange={handleInputChange}
+              >
+                <option selected>Tu habilidad en la cocina</option>
+                <option value="bajo">
+                  Bajo: El colacao cuenta como cocinar
+                </option>
+                <option value="medio">
+                  Medio: Las lentejas no se me queman
+                </option>
+                <option value="avanzado">
+                  Avanzado: David muñoz a mi lado es un mindundi
+                </option>
+              </select>
+              <select
+                className="form-select mb-4"
+                aria-label="Default select example"
+                name="typeDiet"
+                value={user.typeDiet}
+                onChange={handleInputChange}
+              >
+                <option selected>Tipo de dieta</option>
+                <option value="omnivoro">
+                  Omnivoro: como carne y casi todo
+                </option>
+                <option value="flexitariano">
+                  Flexitariano: no excluyo la carne del todo{" "}
+                </option>
+                <option value="vegetariano">Vegetariano</option>
+                <option value="vegano">Vegano</option>
+                <option value="otra">Otra</option>
+              </select>
+              <label htmlFor="objetive" className="form-label">
+                ¿Eres alérgico o intelerante a algún alimento?
+              </label>
+              <select
+                className="form-select mb-4"
+                aria-label="Default select example"
+                name="alergic"
+                value={user.alergic}
+                onChange={handleInputChange}
+              >
+                <option selected>Tipo de alimento</option>
+                <option value="ninguno">Ninguno</option>
+                <option value="huevo">Huevo</option>
+                <option value="marisco">Marisco</option>
+                <option value="lactosa">Lactosa</option>
+                <option value="gluten">Gluten</option>
+                <option value="frutos secos">Frutos secos</option>
+              </select>
+            </FormWizard.TabContent>
+            <FormWizard.TabContent title="Validación" icon="ti-check">
+              <h1>¡Enhorabuena! ¡Has completado todos los pasos!</h1>
+              <p>¡Estás en buenas manos! y no nos las lavamos a menudo</p>
+              <quote>
+                Las buenas recetas son como los buenos amigos: hacen que la vida
+                sea más deliciosa
+              </quote>
+            </FormWizard.TabContent>
+          </FormWizard>
+          {error && <div className="alert alert-danger mt-3">{error}</div>}
+          <style>{`
         @import url("https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css");
       `}</style>
+        </>
+      )}
     </>
   );
 };
